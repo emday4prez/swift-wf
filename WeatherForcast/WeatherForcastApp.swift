00ee34156d10416cc59e38cf3c4cc241def934e5
Emerson Day <emersonday@MBP19.local>
2025-01-26 19:25:01 -0800

    first commit

---
+++ WeatherForcast/WeatherForcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForcastApp.swift
+//  WeatherForcast
+//
+//  Created by Emerson Day on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherForcastTests/WeatherForcastTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForcastTests.swift
+//  WeatherForcastTests
+//
+//  Created by Emerson Day on 1/26/25.
+//
+
+import Testing
+@testable import WeatherForcast
+
+struct WeatherForcastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
