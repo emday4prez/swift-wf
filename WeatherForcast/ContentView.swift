8700b4590cf792bf3f445971c2c1af2a021d1543
Emerson <emerson@MacBookPro.lan>
2025-01-30 18:01:51 -0800

    customize views tutorial

--- WeatherForcast/ContentView.swift
+++ WeatherForcast/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            DayForecast(day: "Mon", high: 70, low: 60)
-            DayForecast(day: "Tue", high:60, low: 40)
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 60)
+            DayForecast(day: "Tue", isRainy: true, high:60, low: 40)
        
         }
     }
@@ -23,15 +23,37 @@ struct ContentView: View {
 
 struct DayForecast: View {
     let day: String
+    let isRainy: Bool
     let high: Int
     let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        }else{
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
+    
     var body: some View {
         VStack {
             Text(day)
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(Color.yellow)
+                .font(Font.largeTitle)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
             Text("High: \(high)")
+                .fontWeight(Font.Weight.semibold)
             Text("Low: \(low)")
+                .fontWeight(Font.Weight.medium)
+                .foregroundStyle(Color.secondary)
         }
         .padding()
     }
